50654b572f691ab4d30f9617ccf133b59eb10c1a
LAPTOP-3GFDSF3B\quocm <quocmcg41@gmail.com>
2024-04-17 10:06:33 +0700

    học được cách sử dụng dark mode cho website cùng với check box

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: 'jit',
+  purge: [
+    './**/*.html',
+    './**/*.{js,jsx,ts,tsx,vue}'
+  ],
+  darkMode: "class",
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
